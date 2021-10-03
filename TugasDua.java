02c7d432f6601c0acb80451d324c1a67bcb3de90
Joshua Nakalelo <71518816+mygithub-joshua@users.noreply.github.com>
2021-10-03 13:48:13 +0700

    Add files via upload

---
+++ TugasDua.java
@@ -0,0 +1,14 @@
+package com.tutorial;
+
+public class TugasDua {
+    public static void main(String[] args) {
+        int tahunLahir = 2000;
+        int tahunSekarang = 2020;
+        int usia = tahunSekarang - tahunLahir;
+
+        System.out.println("Kaila lahir di tahun " + tahunLahir + ". ");
+
+        System.out.println("Sekarang sudah tahun " + tahunSekarang);
+        System.out.println("Berarti, usia Kaila sekarang adalah " + usia + " tahun");
+    }
+}
